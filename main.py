b93c64a9f7aaab828c3b08c5889b3a9ec01f30b0
Olve Drageset <olve.drageset@gmail.com>
2018-12-14 19:49:42 +0100

    completed experiment execution code

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 from auction import Input
 from auction import Auction
 import numpy as np
-# here test code can be placed
+
+
+def round_average_market_prices(market_prices):
+    round_averages = []
+    for round_outcome in market_prices:
+        round_averages.append(sum(round_outcome) / float(len(round_outcome)))
+    return round_averages
+
+
+def seller_average_market_prices(market_prices):
+    seller_averages = []
+    for seller_outcome in market_prices.T:
+        seller_averages.append(sum(seller_outcome) / float(len(seller_outcome)))
+    return seller_averages
+
+
+def process_data(market_prices, buyer_profits, seller_profits):
+    market_prices = np.reshape(market_prices, (auction_parameters.num_round, auction_parameters.num_seller))
+    round_averages = round_average_market_prices(market_prices)
+    seller_averages = seller_average_market_prices(market_prices)
+    avg_buyer_profit_per_round = (sum(buyer_profits) / float(len(buyer_profits))) / auction_parameters.num_round
+    avg_seller_profit_per_round = (sum(seller_profits) / float(len(seller_profits))) / auction_parameters.num_round
+    avg_market_price = sum(map(sum, market_prices)) / (auction_parameters.num_seller * auction_parameters.num_round)
+    return market_prices, round_averages, seller_averages, avg_buyer_profit_per_round, avg_seller_profit_per_round, avg_market_price
+
 
 auction_parameters = Input()
-
 test = Auction(auction_parameters)
 market_prices, buyer_profits, seller_profits = test.run()
-
-market_prices = np.reshape(market_prices, (auction_parameters.num_round, auction_parameters.num_seller))
-round_averages = []
-seller_averages = []
-
-for round_outcome in market_prices:
-    round_averages.append(sum(round_outcome) / float(len(round_outcome)))
-
-for seller_outcome in market_prices.T:
-    seller_averages.append(sum(seller_outcome) / float(len(seller_outcome)))
-
-np.reshape([0, 0, 1, 1, 2, 2, 3, 3], (2, 4))
-# array([[0, 0, 1, 1],
-#        [2, 2, 3, 3]])
+market_prices, round_avgs, seller_avgs, avg_buyer_profit_per_round, avg_seller_profit_per_round, avg_market_price = process_data(market_prices, buyer_profits, seller_profits)
 
 print(f"Market Prices:")
 for i, round_outcome in enumerate(market_prices):
     print(f"round {i}: {round_outcome}")
 print()
 
-avg_buyer_profit_per_round = (sum(buyer_profits) / float(len(buyer_profits))) / auction_parameters.num_round
-avg_seller_profit_per_round = (sum(seller_profits) / float(len(seller_profits))) / auction_parameters.num_round
-
 print(f"Buyer profits for all {auction_parameters.num_round} rounds: \n{buyer_profits}\n"
       f"AVG Buyer profits per round: \n{avg_buyer_profit_per_round}\n"
       f"Seller profits for all {auction_parameters.num_round} rounds: \n{seller_profits}\n"
-      f"AVG Seller profits per round: {avg_seller_profit_per_round}\n")
+      f"AVG Seller profits per round: {avg_seller_profit_per_round}\n"
+      f"AVG market price: {avg_market_price}\n")
--- tests.py
+++ tests.py
@@ -1,34 +1,81 @@
 from auction import Input
 from auction import Auction
 import numpy as np
+import main
 
 """ In this part we construct lists of legal parameter values for each variable parameter to the auction model """
-min_number_buyers = 2
-max_number_buyers = 21
-num_buyers_param_list = list(range(min_number_buyers, max_number_buyers))
-
 min_number_sellers = 1
-max_number_sellers = 20
-num_sellers_param_list = list(range(min_number_sellers, max_number_sellers))
+max_number_sellers = 10
+num_sellers_param_list = list(range(min_number_sellers, max_number_sellers+1))
+
+min_number_buyers = min_number_sellers + 1
+max_number_buyers = (max_number_sellers + 1) * 2
+num_buyers_param_list = list(range(min_number_buyers, max_number_buyers+1))
 
 min_number_itemtype = 1
 max_number_itemtype = max_number_sellers
-num_itemtype_param_list = list(range(min_number_itemtype, max_number_itemtype))
+num_itemtype_param_list = list(range(min_number_itemtype, max_number_itemtype+1))
 
 min_number_rounds = 1
 max_number_rounds = 20
-num_rounds_param_list = list(range(min_number_itemtype, max_number_itemtype))
+num_rounds_param_list = list(range(min_number_rounds, max_number_rounds+1))
 
 min_s_max = 1  # The s_max is the maximal starting price for auctions
 max_s_max = 10
-s_max_param_list = list(range(min_number_itemtype, max_number_itemtype))
+s_max_param_list = list(range(min_s_max, max_s_max+1))
 
 min_fine = 0.1  # The part of the starting price used as a fine
-max_fine = 1  # All of the starting price is used as a fine, in which case you get no money back for returning the item
+max_fine = 1.2  # All of the starting price is used as a fine, in which case you get no money back for returning item
 fine_param_list = np.arange(min_fine, max_fine, 0.1)
 # End of parameter list generation
 
-auction_parameters = Input()
+"""
+For each number of sellers defined in the parameter list above, run experiments with 1-10 more buyers than sellers,
+and with a number of item types up to the amount of sellers.
+"""
+buyers_sellers_rounds_results = []
+for number_of_sellers in num_sellers_param_list:
+    auction_parameters = Input()
+    auction_parameters.num_seller = number_of_sellers
+    for number_of_buyers in range(number_of_sellers+1, number_of_sellers + 10):
+        auction_parameters.num_buyer = number_of_buyers
+        for number_of_item_types in range(min_number_itemtype, number_of_sellers):
+            auction_parameters.num_itemtype = number_of_item_types
+            test = Auction(auction_parameters)
+            market_prices, buyer_profits, seller_profits = test.run()
+            market_prices, round_avgs, seller_avgs, avg_buyer_profit_per_round, avg_seller_profit_per_round, avg_market_price = main.process_data(
+                market_prices, buyer_profits, seller_profits)
+            results.append([auction_parameters, avg_buyer_profit_per_round, avg_seller_profit_per_round, avg_market_price])
 
-test = Auction(auction_parameters)
-market_prices, buyer_profits, seller_profits = test.run()
+
+auction_parameters = Input()
+rounds_results = []
+for number_of_rounds in num_rounds_param_list:
+    auction_parameters.num_round = number_of_rounds
+    test = Auction(auction_parameters)
+    market_prices, buyer_profits, seller_profits = test.run()
+    market_prices, round_avgs, seller_avgs, avg_buyer_profit_per_round, avg_seller_profit_per_round, avg_market_price = main.process_data(
+        market_prices, buyer_profits, seller_profits)
+    rounds_results.append([auction_parameters, avg_buyer_profit_per_round, avg_seller_profit_per_round, avg_market_price])
+
+
+auction_parameters = Input()
+s_max_results = []
+for s_max in s_max_param_list:
+    auction_parameters.s_max = s_max
+    test = Auction(auction_parameters)
+    market_prices, buyer_profits, seller_profits = test.run()
+    market_prices, round_avgs, seller_avgs, avg_buyer_profit_per_round, avg_seller_profit_per_round, avg_market_price = main.process_data(
+        market_prices, buyer_profits, seller_profits)
+    s_max_results.append([auction_parameters, avg_buyer_profit_per_round, avg_seller_profit_per_round, avg_market_price])
+
+
+auction_parameters = Input()
+fine_results = []
+for fine in fine_param_list:
+    auction_parameters.fine = fine
+    test = Auction(auction_parameters)
+    market_prices, buyer_profits, seller_profits = test.run()
+    market_prices, round_avgs, seller_avgs, avg_buyer_profit_per_round, avg_seller_profit_per_round, avg_market_price = main.process_data(
+        market_prices, buyer_profits, seller_profits)
+    fine_results.append([auction_parameters, avg_buyer_profit_per_round, avg_seller_profit_per_round, avg_market_price])
